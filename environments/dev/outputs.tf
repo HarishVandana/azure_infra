55c97047ce0d4a62dd303f02fdb6c1e9b1b6f22a
HarishVandana <harish.vandana.01@gmail.com>
2025-04-11 13:54:28 +0530

    Update outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,19 +1,1 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
 
-output "vnet_name" {
-  value = module.vnet.vnet_name
-}
-
-output "subnet_id" {
-  value = module.vnet.subnet_id
-}
-
-output "vm_name" {
-  value = azurerm_windows_virtual_machine.vm.name
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.devsa.name
-}
