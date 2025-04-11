00d13d2813889aef0035f33842b0d6d50e5a46b0
HarishVandana <harish.vandana.01@gmail.com>
2025-04-11 13:52:37 +0530

    Update variables.tf

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -1,24 +1,12 @@
-variable "prefix" {
-  type        = string
-  description = "Prefix used for naming resources"
+variable "vnet_name" {}
+variable "resource_group_name" {}
+variable "location" {}
+variable "address_space" {
+  type = list(string)
 }
-
-variable "env" {
-  type        = string
-  description = "Environment name (e.g., dev, stage, prod)"
+variable "subnets" {
+  type = map(list(string))
 }
-
-variable "location" {
-  type        = string
-  description = "Azure region to deploy resources into"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group in which to create the VNET"
-}
-
 variable "tags" {
   type = map(string)
-  description = "Tags to apply to resources"
 }
